750d253ad6a1614f979f0471b96252867dda22e5
Nicole Li <li.nicole160@gmail.com>
2018-07-16 13:00:30 -0700

    toolbar update

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,111 @@
-import React from 'react';
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+
+import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+
+class HeadlinesPicker extends Component {
+  componentDidMount() {
+    setTimeout(() => { window.addEventListener('click', this.onWindowClick); });
   }
 
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(
-      this.state.editorState,
-      'BOLD'
-    ));
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
   }
+
+  onWindowClick = () =>
+    // Call `onOverrideContent` again with `undefined`
+    // so the toolbar can show its regular content again.
+    this.props.onOverrideContent(undefined);
+
+  render() {
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+    return (
+      <div>
+        {buttons.map((Button, i) => // eslint-disable-next-line
+          <Button key={i} {...this.props} />
+        )}
+      </div>
+    );
+  }
+}
+
+class HeadlinesButton extends Component {
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
 
   render() {
     return (
-      <div id='content'>
-        <h1>Draft.js Editor</h1>
-        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-        <div className='editor'>
+      <div className='headlineButtonWrapper'>
+        <button onClick={this.onClick} className='headlineButton'>
+          H
+        </button>
+      </div>
+    );
+  }
+}
+
+const toolbarPlugin = createToolbarPlugin({
+  structure: [
+    BoldButton,
+    ItalicButton,
+    UnderlineButton,
+    CodeButton,
+    Separator,
+    HeadlinesButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton
+  ]
+});
+const { Toolbar } = toolbarPlugin;
+const plugins = [toolbarPlugin];
+const text = 'In this editor a toolbar shows up once you select part of the text …';
+
+export default class CustomToolbarEditor extends Component {
+
+  state = {
+    editorState: createEditorStateWithText(text),
+  };
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  focus = () => {
+    this.editor.focus();
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='editor' onClick={this.focus}>
           <Editor
             editorState={this.state.editorState}
             onChange={this.onChange}
+            plugins={plugins}
+            ref={(element) => { this.editor = element; }}
           />
+          <Toolbar />
         </div>
       </div>
     );
