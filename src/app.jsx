dfaba35e1ce2a86cc07dcc0b6e535ac2976f570c
Nicole Li <li.nicole160@gmail.com>
2018-07-16 10:55:16 -0700

    added bold button

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'BOLD'
+    ));
+  }
+
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <div id='content'>
+        <h1>Draft.js Editor</h1>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <div className='editor'>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+          />
+        </div>
+      </div>
+    );
   }
 }
