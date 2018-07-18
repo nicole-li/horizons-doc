2ad53271f4e82d6d6d27d9f0cfbfa2e2a5eb8374
Nicole Li <li.nicole160@gmail.com>
2018-07-18 10:04:19 -0700

    fix register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,34 +9,23 @@ export default class Login extends React.Component {
     }
   }
 
-  login = () => {
-    fetch('/login', {
+  login = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/login', {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
       method: 'POST',
-      data: {
+      body: {
         username: this.state.username,
         password: this.state.password
       }
     })
-    .then(resp => resp.json())
-    .then(json => {
-      if (json.success) {
-        this.props.redirect('Home')
-      }
-    })
-  }
-
-
-  login = (e) => {
-    e.preventDefault;
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
-      data: {
-      }
-    })
     .then((resp) => resp.json())
     .then((json) => {
+      console.log(json);
       if (json.success) {
-        this.props.redirect('Document')
+        this.props.redirect('Home')
       }
       else {
         alert('Could not Login.')
@@ -49,8 +38,14 @@ export default class Login extends React.Component {
       <div>
         <h2 className="h2">Login</h2>
         <div className="form">
-          <p>Username: <input onChange={(e) => this.setState({username: e.target.value})} type="text"/></p>
-          <p>Password: <input onChange={(e) => this.setState({password: e.target.value})} type="text"/></p>
+          <p>Username: <input
+            onChange={(e) => this.setState({username: e.target.value})}
+            value={this.state.username}
+            type="text"/></p>
+          <p>Password: <input
+            onChange={(e) => this.setState({password: e.target.value})}
+            value={this.state.password}
+            type="text"/></p>
           <button onClick={this.login} className="button">Login</button>
         </div>
         <button className="button" onClick={()=>{this.props.redirect('Home')}}>Home</button>
